549ba7bba1bb53ed5b5de13318166738e4b692ff
AresCodes01 <wares.ahmad01@gmail.com>
2025-09-17 10:12:22 +0200

    Service Container

--- tcheck/controllers/notes/store.php
+++ tcheck/controllers/notes/store.php
@@ -2,17 +2,16 @@
 
 
 use Core\Validator;
+use Core\App;
 use Core\Database;
 
-$config = require base_path("config.php");
-$db = new Database($config["database"]);
-
+$db = App::resolve(Database::class);
 $errors = [];
 
 if (!Validator::string($_POST["body"], 1, 1000)) {
     $errors['body'] = 'A body of no more than 1000 characters is required';
 }
-if (! empty($errors)) {
+if (!empty($errors)) {
     return view("notes/create.view.php", [
         'heading' => 'Create Note',
         'errors' => $errors
@@ -20,11 +19,11 @@ if (! empty($errors)) {
 }
 
 
-    $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
-        'body' => $_POST['body'],
-        'user_id' => 1
-    ]);
+$db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
+    'body' => $_POST['body'],
+    'user_id' => 1
+]);
 
-    header('Location: /notes');
-    die();
+header('Location: /notes');
+die();
 
