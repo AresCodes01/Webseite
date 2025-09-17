78f60b43e2dcf59472388cbcc6b46abfc042cfe0
AresCodes01 <wares.ahmad01@gmail.com>
2025-09-17 16:09:14 +0200

    Log in Screen part

---
+++ tcheck/views/registration/create.view.php
@@ -0,0 +1,101 @@
+<?php require base_path('views/partials/head.php') ?>
+<?php require base_path('views/partials/nav.php') ?>
+
+    <main>
+        <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+            <div class="w-full max-w-md space-y-8">
+                <div>
+                    <img class="mx-auto h-12 w-auto" src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500""
+                         alt="Your Company">
+                    <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Register for a new
+                        account</h2>
+                </div>
+
+                <form class="mt-8 space-y-6" action="/register" method="POST">
+                    <div class="-space-y-px rounded-md shadow-sm">
+                        <div>
+                            <label for="email" class="sr-only">Email address</label>
+                            <input id="email" name="email" type="email" autocomplete="email" required
+                                   class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                                   placeholder="Email address">
+                        </div>
+
+                        <div>
+                            <label for="password" class="sr-only">Password</label>
+                            <input id="password" name="password" type="password" autocomplete="current-password" required
+                                   class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                                   placeholder="Password">
+                        </div>
+                    </div>
+
+                    <div>
+                        <button type="submit"
+                                class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                        >
+                            Register
+                        </button>
+                    </div>
+
+                    <ul>
+                        <?php if (isset($errors['email'])) : ?>
+                            <li class="text-red-500 text-xs mt-2"><?= $errors['email'] ?></li>
+                        <?php endif; ?>
+
+                        <?php if (isset($errors['password'])) : ?>
+                            <li class="text-red-500 text-xs mt-2"><?= $errors['password'] ?></li>
+                        <?php endif; ?>
+                    </ul>
+                </form>
+            </div>
+        </div>
+    </main>
+
+<?php require base_path('views/partials/footer.php') ?>
+
+<!--
+  This example requires updating your template:
+
+  ```
+  <html class="h-full bg-gray-900">
+  <body class="h-full">
+  ```
+-->
+<div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+        <img src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" class="mx-auto h-10 w-auto" />
+        <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-white">Sign in to your account</h2>
+    </div>
+
+    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form action="#" method="POST" class="space-y-6">
+            <div>
+                <label for="email" class="block text-sm/6 font-medium text-gray-100">Email address</label>
+                <div class="mt-2">
+                    <input id="email" type="email" name="email" required autocomplete="email" class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
+                </div>
+            </div>
+
+            <div>
+                <div class="flex items-center justify-between">
+                    <label for="password" class="block text-sm/6 font-medium text-gray-100">Password</label>
+                    <div class="text-sm">
+                        <a href="#" class="font-semibold text-indigo-400 hover:text-indigo-300">Forgot password?</a>
+                    </div>
+                </div>
+                <div class="mt-2">
+                    <input id="password" type="password" name="password" required autocomplete="current-password" class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
+                </div>
+            </div>
+
+            <div>
+                <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Sign in</button>
+            </div>
+        </form>
+
+        <p class="mt-10 text-center text-sm/6 text-gray-400">
+            Not a member?
+            <a href="#" class="font-semibold text-indigo-400 hover:text-indigo-300">Start a 14 day free trial</a>
+        </p>
+    </div>
+</div>
+
