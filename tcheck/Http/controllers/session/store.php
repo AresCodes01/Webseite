82d8328c7c2d06a91e3e719479cfbb7c0f3cbbf0
AresCodes01 <wares.ahmad01@gmail.com>
2025-09-23 11:39:25 +0200

    Refactoring the LoginForm Validation

--- tcheck/Http/controllers/session/store.php
+++ tcheck/Http/controllers/session/store.php
@@ -3,23 +3,19 @@
 use Core\App;
 use Core\Database;
 use Core\Validator;
+use Http\Forms\LoginForm;
 
 $db = App::resolve(Database::class);
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-$errors = [];
-if (!Validator::email($email)) {
-    $errors['email'] = "Please provide a valid email address.";
-}
+$form = new LoginForm();
+if (! $form->validate($email, $password)) {
 
-if (!Validator::string($password)) {
-    $errors['password'] = "Please provide a valid password.";
-}
-if (!empty($errors)) {
     return view("session/create.view.php", [
-        "errors" => $errors
+        "errors" => $form->errors()
     ]);
+
 }
 
 $user = $db->query("select * from users where email =:email", [
